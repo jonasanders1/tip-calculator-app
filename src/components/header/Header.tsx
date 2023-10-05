b7b883ce549de2c35428410c44ccc2bad91657ff
Jonas Andersen <94622735+jonasanders1@users.noreply.github.com>
2023-10-05 20:45:57 +0200

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import "./header.css"
+
+const Header = () => {
+
+  return (
+    <div className="header__container">
+      <div className="header__content">
+        <h1 className="header-title">Spli</h1>
+        <h1 className="header-title">tter</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
